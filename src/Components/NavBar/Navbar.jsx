69be06c48aebd913db6ce336507fc1448df59f02
TrippyNitish <nitish.trippybug@gmail.com>
2023-09-07 14:24:21 +0530

    better ui 5

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,55 +1,70 @@
-import React, { useEffect, useState,  } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
-import Cookies from 'js-cookie';
-import './Navbar.css'
-import Sidebar from '../Sidebar/SideBar';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { setLogout } from '../../redux/reducers';
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Cookies from "js-cookie";
+import "./Navbar.css";
+import Sidebar from "../Sidebar/SideBar";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { setLogout } from "../../redux/reducers";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const user = useSelector((state) => state.user);
 
-    const user = useSelector((state) => state.user)
+  const [isSideBarShow, setIsSideBarShow] = useState(true);
 
-    const [isSideBarShow,setIsSideBarShow]=useState(true)
+  const handleBarButtonClick = () => {
+    setIsSideBarShow(!isSideBarShow);
+  };
 
-    const handleBarButtonClick=()=>{
-        setIsSideBarShow(!isSideBarShow)
-    }
+  const handleLogOut = (e) => {
+    dispatch(setLogout());
+    Cookies.remove("userToken");
+    navigate("/");
+  };
 
-    const handleLogOut = (e) => {
-        dispatch(setLogout())
-        Cookies.remove('userToken');
-        navigate("/")
-    }
+  useEffect(() => {
+    if (!user.userName) navigate("/");
+  }, []);
 
-    useEffect(() => {
-        if (!user.userName)
-            navigate("/")
-    }, [])
-
-    return (
-        <div className="navBar">
-            <div className='isSideBarButtonShow' onClick={()=>handleBarButtonClick()}>{(isSideBarShow? <span>&equiv;</span>:<span>&times;</span>)}</div>
-            {!isSideBarShow && <div className='navSideBarShown'>
-                <Sidebar/>
-            </div>}
-            <div className="navBarComponents">
-               {user.designation!="company"  && <div>{`Credits : ${user.credits != null ? user.credits : "Infinit"}`}  </div>}
-            </div>
-            <div> Game </div>
-            <div className="navBarComponents">
-                <AccountCircleIcon/>
-                <div className="welcome">{` ${user.userName}(${user.designation})`}  </div>
-                <button className="navBarButtons deleteButton" onClick={(e) => handleLogOut(e)}>LogOut</button>
-            </div>
+  return (
+    <div className="navBar">
+      <div
+        className="isSideBarButtonShow"
+        onClick={() => handleBarButtonClick()}
+      >
+        {isSideBarShow ? <span>&equiv;</span> : <span>&times;</span>}
+      </div>
+      {!isSideBarShow && (
+        <div className="navSideBarShown">
+          <Sidebar />
         </div>
+      )}
+      <div className="navBarComponents">
+        {user.designation != "company" && (
+          <div>
+            {`Credits : ${user.credits != null ? user.credits : "Infinit"}`}{" "}
+          </div>
+        )}
+      </div>
+      <div> Game </div>
+      <div className="navBarComponents">
+        <AccountCircleIcon />
+        <div>
+          <div className="welcome">{` ${user.userName}`} </div>
+          <div>{`(${user.designation})`}</div>
+        </div>
+        <button
+          className="navBarButtons deleteButton"
+          onClick={(e) => handleLogOut(e)}
+        >
+          LogOut
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default NavBar
+export default NavBar;
